8123670c7d4fb74dff8f3cc24336aaf896228ed6
GoingRight <choibin1008@naver.com>
2025-03-10 16:30:08 +0900

    [Feat] 점프대 구현

---
+++ Assets/02.Scripts/JumpArea.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpArea : MonoBehaviour
+{
+    public float jumpPower;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+            rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+    }
+}
